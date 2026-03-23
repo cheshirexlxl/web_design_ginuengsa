d9669acadae07b9c8eed68f79bac49586a23d744
cheshirexlxl <cheshire.xlxl@gmail.com>
2026-03-23 21:42:51 +0900

    3월 23일 연습

---
+++ 연습/TEST-A-2/script/script.js
@@ -0,0 +1,33 @@
+$(function() {
+    $(".main, .sub").on("mouseenter", function() {
+        $(".sub").stop().slideDown()
+    })
+    $(".main, .sub").on("mouseleave", function() {
+        $(".sub").stop().slideUp()
+    })
+
+    const speed = 500;
+    const time = 3000;
+    const $slide = $(".slide")
+    const $container = $(".slide-container")
+    const size = $slide.width();
+    const count = $slide.length;
+    $container.width(size * count)
+
+    setInterval(function() {
+        $container.animate({
+            left: -size
+        }, speed, function() {
+            $container.css("left", "0")
+            $container.append($(".slide").first())
+        })
+    }, time)
+
+    $(".btn-modal").on("click", function() {
+        $("#modal").show();
+    })
+     $(".close").on("click", function() {
+        $("#modal").hide();
+    })
+
+})
